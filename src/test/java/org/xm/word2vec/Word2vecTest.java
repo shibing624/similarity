250af60e5dd0c901dc5aa9ea18061bc84fe1c59f
xuming <shibing624@126.com>
2016-11-23 09:21:46 +0800

    update word2vec test。xuming 20161123

--- src/test/java/org/xm/word2vec/Word2vecTest.java
+++ src/test/java/org/xm/word2vec/Word2vecTest.java
@@ -9,16 +9,18 @@ import java.util.List;
  * @author xuming
  */
 public class Word2vecTest {
-    String outputPath="";
+    String outputModelPath = "";
+
     @Before
-    public void before()throws Exception{
-        outputPath = Word2vec.trainModel("data/seg_result.txt");
-        System.out.println("outputPath："  +outputPath);
+    public void before() throws Exception {
+        outputModelPath = Word2vec.trainModel("data/seg_result.txt");
+        System.out.println("outputModelPath：" + outputModelPath);
     }
+
     @Test
     public void test() throws Exception {
-        List<String> result = Word2vec.getHomoionym(outputPath, "贸易", 10);
-        System.out.println("贸易 近似词："  +result);
+        List<String> result = Word2vec.getHomoionym(outputModelPath, "贸易", 10);
+        System.out.println("贸易 近似词：" + result);
     }
 
 }
