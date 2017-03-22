35f00e3ef3c9b384d0a9c66690a2d378032f7f34
xuming <shibing624@126.com>
2017-03-22 15:47:44 +0800

    update word2vec test。xuming 20170322

--- src/test/java/org/xm/word2vec/Word2vecTest.java
+++ src/test/java/org/xm/word2vec/Word2vecTest.java
@@ -1,6 +1,5 @@
 package org.xm.word2vec;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -9,18 +8,30 @@ import java.util.List;
  * @author xuming
  */
 public class Word2vecTest {
-    String outputModelPath = "";
 
-    @Before
-    public void before() throws Exception {
-        outputModelPath = Word2vec.trainModel("data/seg_result.txt");
+    @Test
+    public void trainModel() throws Exception {
+        String outputModelPath = Word2vec.trainModel("data/jinyong_novels_split.txt");
         System.out.println("outputModelPath：" + outputModelPath);
     }
 
     @Test
-    public void test() throws Exception {
-        List<String> result = Word2vec.getHomoionym(outputModelPath, "贸易", 10);
-        System.out.println("贸易 近似词：" + result);
+    public void testHomoionym() throws Exception {
+        List<String> result = Word2vec.getHomoionym("data/jinyong_novels_split.txt.model", "道路", 10);
+        System.out.println("道路 近似词：" + result);
+    }
+
+    @Test
+    public void testHomoionymName() throws Exception {
+        String model = "data/jinyong_novels_split.txt.model";
+        List<String> result = Word2vec.getHomoionym(model, "乔峰", 10);
+        System.out.println("乔峰 近似词：" + result);
+
+        List<String> result2 = Word2vec.getHomoionym(model, "阿朱", 10);
+        System.out.println("阿朱 近似词：" + result2);
+
+        List<String> result3 = Word2vec.getHomoionym(model, "降龙十八掌", 10);
+        System.out.println("降龙十八掌 近似词：" + result3);
     }
 
 }
