8496be15f5016c3c589d421ffce5c7994ed0b783
xuming <507153809@qq.com>
2017-06-27 10:31:47 +0800

    更新DiceCoeffieient 相似度计算方法。xuming 20170627

--- src/main/java/org/xm/similarity/text/JaroWinklerDistanceTextSimilarity.java
+++ src/main/java/org/xm/similarity/text/JaroWinklerDistanceTextSimilarity.java
@@ -98,14 +98,8 @@ public class JaroWinklerDistanceTextSimilarity extends JaroDistanceTextSimilarit
         double score1pk1 = textSimilarity.getSimilarity(text1, text1);
         double score1pk2 = textSimilarity.getSimilarity(text1, text2);
         double score1pk3 = textSimilarity.getSimilarity(text1, text3);
-        double score2pk2 = textSimilarity.getSimilarity(text2, text2);
-        double score2pk3 = textSimilarity.getSimilarity(text2, text3);
-        double score3pk3 = textSimilarity.getSimilarity(text3, text3);
         System.out.println(text1 + " 和 " + text1 + " 的相似度分值：" + score1pk1);
         System.out.println(text1 + " 和 " + text2 + " 的相似度分值：" + score1pk2);
         System.out.println(text1 + " 和 " + text3 + " 的相似度分值：" + score1pk3);
-        System.out.println(text2 + " 和 " + text2 + " 的相似度分值：" + score2pk2);
-        System.out.println(text2 + " 和 " + text3 + " 的相似度分值：" + score2pk3);
-        System.out.println(text3 + " 和 " + text3 + " 的相似度分值：" + score3pk3);
     }
 }
