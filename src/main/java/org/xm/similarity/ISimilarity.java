727ac145c99e39247d1aa175a5b14839a6573b11
xuming <507153809@qq.com>
2017-07-06 09:17:38 +0800

    更新相似度接口。xuming 20170706

--- src/main/java/org/xm/similarity/ISimilarity.java
+++ src/main/java/org/xm/similarity/ISimilarity.java
@@ -6,5 +6,12 @@ package org.xm.similarity;
  * @author xuming
  */
 public interface ISimilarity {
+    /**
+     * 计算相似度
+     *
+     * @param word1 词语1
+     * @param word2 词语2
+     * @return 相似度值
+     */
     double getSimilarity(String word1, String word2);
 }
